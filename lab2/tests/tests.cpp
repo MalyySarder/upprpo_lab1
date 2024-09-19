2a9d2bd19b06b1106a928c14d394f4e5f97c7039
MalyySarder1 <niknikivan35@gmail.com>
2024-09-19 15:10:33 +0300

    tests

--- lab2/tests/tests.cpp
+++ lab2/tests/tests.cpp
@@ -5,7 +5,7 @@ int main() {
     BigArray arr(10);
     arr.setValue(0, 42);
     arr.setValue(1, 9);
-    assert(arr.getValue(0) == 55);
-    assert(arr.getValue(1) == 44);
+    assert(arr.getValue(0) == 42);
+    assert(arr.getValue(1) == 9);
     return 0;
 }
